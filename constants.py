5155c659f438e6680b4b71817c491226f8e59c3b
Violet <crystalbomb64@hotmail.com>
2023-01-18 11:23:26 +0400

    add rarity, profession, and stat constants to transform to readable forms

--- constants.py
+++ constants.py
@@ -1,4 +1,30 @@
 CRYSTALVALE_RPC = "https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc"
 SERENDALE_RPC = "https://cypress.fandom.finance/archive"
-CRYSTALVALE_HERO_CONTRACT_ADDRESS = '0xEb9B61B145D6489Be575D3603F4a704810e143dF'
-SERENDALE_HERO_CONTRACT_ADDRESS = '0x268CC8248FFB72Cd5F3e73A9a20Fa2FF40EfbA61'
+CRYSTALVALE_HERO_CONTRACT_ADDRESS = "0xEb9B61B145D6489Be575D3603F4a704810e143dF"
+SERENDALE_HERO_CONTRACT_ADDRESS = "0x268CC8248FFB72Cd5F3e73A9a20Fa2FF40EfbA61"
+
+rarity = {
+    0: "common",
+    1: "uncommon",
+    2: "rare",
+    3: "legendary",
+    4: "mythic",
+}
+
+professions = {
+    0: 'mining',
+    2: 'gardening',
+    4: 'fishing',
+    6: 'foraging',
+}
+
+stats = {
+    0: 'strength',
+    2: 'agility',
+    4: 'intelligence',
+    6: 'wisdom',
+    8: 'luck',
+    10: 'vitality',
+    12: 'endurance',
+    14: 'dexterity'
+}
