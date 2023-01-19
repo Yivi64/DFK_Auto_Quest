a739662d9bbd2ae93de99d317e455c46b939e239
Violet <crystalbomb64@hotmail.com>
2023-01-19 22:35:21 +0400

    add contracts for all CV/SR quests

--- constants.py
+++ constants.py
@@ -1,7 +1,11 @@
 CRYSTALVALE_RPC = "https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc"
 SERENDALE_RPC = "https://cypress.fandom.finance/archive"
+
 CRYSTALVALE_HERO_CONTRACT_ADDRESS = "0xEb9B61B145D6489Be575D3603F4a704810e143dF"
 SERENDALE_HERO_CONTRACT_ADDRESS = "0x268CC8248FFB72Cd5F3e73A9a20Fa2FF40EfbA61"
+
+CRYSTALVALE_QUEST_CONTRACT_ADDRESS = "0xE9AbfBC143d7cef74b5b793ec5907fa62ca53154"
+SERENDALE_QUEST_CONTRACT_ADDRESS = "0x8dc58d6327E1f65b18B82EDFb01A361f3AAEf624"
 
 rarity = {
     0: "common",
@@ -27,4 +31,41 @@ stats = {
     10: "vitality",
     12: "endurance",
     14: "dexterity"
+}
+
+realm = {
+    0: "SER",
+    1: "CRY"
+}
+
+cry_quest_contracts = {
+    "gold": "0x75912145f5cFEfb980616FA47B2f103210FaAb94",
+    "token": "0x98b3C85ac3cC3EF36Ff25A9229857AbACE3e7410",
+    "fishing": "0x407ab39B3675f29A719476af6eb3B9E5d93969E6",
+    "foraging": "0xAd51199B453075C73FA106aFcAAD59f705EF7872",
+    "gardening": "N/A",
+    "strength": "0xb8828c687Fb1C875D5acb4281C5CDf9F49fA4637",
+    "intelligence": "0xD8cCf866959830a8E397442B5F7DDD790F230962",
+    "wisdom": "0x0832A218c2202088A1800D424248fC689ae74600",
+    "luck": "0x81fA8a2bfcd703dc83c5d4bEE1075899448A5CdE",
+    "agility": "0x801b7296f106d8818DA1D04Ed769e5a76e8911fe",
+    "vitality": "0xE3edf52D33F2BB05DBdA5BA73903E27a9B9b7e9d",
+    "endurance": "0xBD391e4641E1bce989a246602EcDC746efA9d845",
+    "dexterity": "0x9ec92963d0387bA57D5f2D505319b1c135C6f1D3"
+}
+
+ser_quest_contracts = {
+    "gold": "0x46F036B26870188aD69877621815238D2b81bef1",
+    "token": "0x20B274262FA6da57B5Ff90498EC373c0266eF901",
+    "fishing": "0x0E7a8b035eF2FA0183a2680458818256424Bd60B",
+    "foraging": "0x54DaD24dDc2cC6E7616438816DE0EeFCad79B625",
+    "gardening": "N/A",
+    "strength": "0xF2143c7c8Dfca976415bDf7d37dfa63aed8Ef741",
+    "intelligence": "0xe606f6548Ae34DA9065B4fee88990F239b445403",
+    "wisdom": "0x80F93836811a9A7721A21D7d8751aFd6A8fC9308",
+    "luck": "0x5C01d797d0Cc3D79c01ef98f7ffAe25E4dCEB400",
+    "agility": "0x378052bbc8D2E1819194802b8A990E7Ae43655bA",
+    "vitality": "0x89a60d8B332ce2Dd3bE8b170c6391F98a03a665F",
+    "endurance": "0x058282847F1C8E893edcdfea5df6eb203ECA7832",
+    "dexterity": "0x8F3acf63fd09ceCD1F387B7bC45bc245f43D4B5e"
 }
