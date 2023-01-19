3732203a13417997b7c99673539d5698deb1f2f6
Violet <crystalbomb64@hotmail.com>
2023-01-19 11:44:31 +0400

    remove alphabet constant

--- constants.py
+++ constants.py
@@ -2,7 +2,6 @@ CRYSTALVALE_RPC = "https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc"
 SERENDALE_RPC = "https://cypress.fandom.finance/archive"
 CRYSTALVALE_HERO_CONTRACT_ADDRESS = "0xEb9B61B145D6489Be575D3603F4a704810e143dF"
 SERENDALE_HERO_CONTRACT_ADDRESS = "0x268CC8248FFB72Cd5F3e73A9a20Fa2FF40EfbA61"
-ALPHABET = "123456789abcdefghijkmnopqrstuvwx"
 
 rarity = {
     0: "common",
